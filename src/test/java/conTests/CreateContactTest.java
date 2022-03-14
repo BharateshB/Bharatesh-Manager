c1c97c23b232d2eacfa88688b5acc34c9fc601b6
Admin <Admin@Dell>
2022-03-14 19:09:19 +0530

    as a manager

---
+++ src/test/java/conTests/CreateContactTest.java
@@ -0,0 +1,22 @@
+package conTests;
+
+import org.testng.annotations.Test;
+
+public class CreateContactTest {
+
+	
+	@Test
+	public void createcont() {
+		
+		System.out.println("contact is created");
+		
+	}
+	
+	@Test
+public void createcont2() {
+		
+		System.out.println("contact2 is created");
+		
+	}
+	
+}
---
+++ src/test/java/orgTests/CreateOrgTest.java
@@ -0,0 +1,20 @@
+package orgTests;
+
+import org.testng.annotations.Test;
+
+public class CreateOrgTest {
+
+	@Test
+	
+	public void createOrg() {
+		
+		String BROWSER = System.getProperty("browser");
+		String URL = System.getProperty("url");
+		
+		
+		System.out.println("browser is:"+BROWSER+" and url is:"+URL);
+		
+		
+	}
+	
+}
